11687887d9214fbdfa9517d587ff5cae92b6a2ba
IvanLuthje <luthjeivan@gmail.com>
2024-09-03 19:19:50 -0300

    add description

--- js/main2.js
+++ js/main2.js
@@ -136,28 +136,6 @@ $(document).ready(function(){
       
     });
 
-function desc(){
-    $.ajax({
-        url: 'https://pokeapi.co/api/v2/pokemon-species/' + id_nombre, 
-        type: "GET",
-        dataType: "json",
-
-        
-        success: function(dato_pokemon){ 
-
-           // $(".datos_pokemon").html("<h1>" + dato_pokemon.name + "</h1><img src='" + dato_pokemon.sprites.front_default + "' alt='" + dato_pokemon.name + "'><p>Peso: " + dato_pokemon.weight + "</p><p>Altura: " + dato_pokemon.height + "cm</p>");
-           //$(".datos_pokemon").html("<h1>" + dato_pokemon.name + "</h1>" + "<div class='pokemon'>" + "<img src='" + dato_pokemon.sprites.front_default + "'>" + "</div>" + "<p>Exp:" + dato_pokemon.base_experience + "</p>" + "<p>Peso:" + dato_pokemon.weight + "Kg</p>"  +  "<p>Altura: " + dato_pokemon.height + "m</p>" + "<button class='compartir' onClick='compartir()'> " + "Compartir" + "</button>");
-           $(".datos_pokemon2").html(descripcion);
-
-        },
-        error: function(xhr, status) {
-            alert("Pokémon " + id_nombre + " no disponible");
-        },
-        
-    });
-}
-    
-
 // Modo responsivo
 
 /* function iniciarMapRes(){
@@ -191,20 +169,30 @@ function desc(){
             if (id_nombre!=''){
                 $(".datos_pokemon").html("<h1>" + dato_pokemon.name + "</h1>"  + "<div class='pokemon'>" + "<img src='" + dato_pokemon.sprites.front_default + "'>" + "</div>"  +  "<p>ID:" + dato_pokemon.id + "</p>"  +  "<p>Exp:" + dato_pokemon.base_experience + "</p>" + "<p>Peso:" + dato_pokemon.weight + "Kg</p>" + "<p>Descripcion:" + dato_pokemon.flavor_text + "</p>"  +  "<p>Altura: " + dato_pokemon.height + "m</p>" + "<button class='compartir' onClick='compartir(this)'> " + "Compartir" + "</button>");
             }
-
-            
-
-            else{
-                
-            }
-            let results = dato_pokemon;
-            console.log(dato_pokemon); 
             },
+
             error: function(xhr, status) {
                 alert("Pokémon " + id_nombre + " no disponible");
             },
             
         });
+
+        $.ajax({
+            url: 'https://pokeapi.co/api/v2/pokemon-species/' + id_nombre, 
+            type: "GET",
+            dataType: "json",
+            
+
+            
+            success: function(dato_pokemon){ 
+                var descripcion = dato_pokemon.flavor_text_entries[26].flavor_text;
+               $(".descripcion").html("<p>" + "Descripción: " + descripcion + "</p>" + "<button class='compartir' onClick='Mostrar(this)'> " + "Compartir" + "</button>");
+            },
+
+            
+        });
+
+
     }
 
     
